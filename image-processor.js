dd3a3c28b8fd78228de5de6ac7951b6cdb4f7e70
GrzywN <karolbinkowski3@proton.me>
2024-12-12 15:51:10 +0100

    fix: make app crash when environmental variables are not set

--- image-processor.js
+++ image-processor.js
@@ -4,6 +4,12 @@ const { default: axios } = require('axios');
 const { chunk } = require('lodash');
 const sharp = require('sharp');
 const mongoose = require('mongoose');
+const { object, string, number, assert } = require('superstruct');
+
+assert(process.env, object({
+  MONGO_URI: string(),
+  DEFAULT_BATCH_SIZE: number(),
+}));
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
